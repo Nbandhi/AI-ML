572348f0de24c284b54efc073b149ad7e71666f2
Nbandhi <Nbandhi@users.noreply.github.com>
2021-04-23 21:02:33 -0400

    Update traffic.py
    Updated traffic.py to add the graphs

--- Road-Signs/traffic.py
+++ Road-Signs/traffic.py
@@ -37,7 +37,24 @@ def main():
     # Evaluate neural network performance
     model.evaluate(x_test,  y_test, verbose=2)
 
-    print(model.summary())
+    # Predict using the model
+    predictions = model.predict(x_test)
+    
+    # Plot the first X test images, their predicted labels, and the true labels.
+    # Color correct predictions in blue and incorrect predictions in red.
+    num_rows = 5
+    num_cols = 3
+
+    plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+    j=0
+    for i in range(91, 106):
+        plt.subplot(num_rows, 2*num_cols, 2*j+1)
+        plot_image(i, predictions[i], y_test, x_test)
+        plt.subplot(num_rows, 2*num_cols, 2*j+2)
+        plot_value_array(i, predictions[i], y_test)
+        j=j+1
+    plt.tight_layout()
+    plt.show()
 
     # Save model to file
     if len(sys.argv) == 3:
@@ -87,21 +104,18 @@ def get_model():
 
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(
-            32, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            32, (5, 5), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
-        #tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-
         tf.keras.layers.Conv2D(
-           64, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+           64, (5, 5), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
         tf.keras.layers.Flatten(),
 
         tf.keras.layers.Dense(128, activation="relu"),
-        #tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Dropout(0.50),
 
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
@@ -114,7 +128,45 @@ def get_model():
 
     return model
 
-    #raise NotImplementedError
+ def plot_image(i, predictions_array, true_label, img):
+    """
+    Plot the image used for predictions along with the predicted label
+    and the actual lable
+    """
+    
+    true_label, img = np.argmax(true_label[i]), img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.imshow(img, cmap=plt.cm.binary)
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+        
+    plt.xlabel("{} {:2.0f}% ({})".format(predicted_label,
+                                100*np.max(predictions_array),
+                                true_label),
+                                color=color)
+    
+ 
+  def plot_value_array(i, predictions_array, true_label):
+        """
+        Plot the predicted values of the image
+        blue - actual value
+        red - predicted value
+        """
+        true_label = np.argmax(true_label[i])
+        plt.grid(False)
+        #plt.xticks(range(43))
+        plt.yticks([])
+        thisplot = plt.bar(range(43), predictions_array, color="#777777")
+        plt.ylim([0, 1])
+        predicted_label = np.argmax(predictions_array)
+        thisplot[predicted_label].set_color('red')
+        thisplot[true_label].set_color('blue')
 
 
 if __name__ == "__main__":
